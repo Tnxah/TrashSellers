bc967d05a9a378f57b348fbbbf32f792e82f689c
Tnxah <tnxah.j@gmail.com>
2022-02-08 12:13:57 +0100

    item manager

---
+++ Assets/Scripts/ItemsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class ItemsManager : MonoBehaviour
+{
+    private Random rnd = new Random();
+    private int time = 5;
+    private float spawnChance = 50;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SlowUpdate());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SlowUpdate()
+    {
+        while (true) { 
+        var rand = rnd.Next(0, 100);
+        print(rand);
+        if (rand < spawnChance)
+        {
+            print("Random item");
+        }
+        yield return new WaitForSeconds(5);
+    }
+
+    }
+}
